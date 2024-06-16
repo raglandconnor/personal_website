09a2d53981f64da25d6aa6b59f463e165dc53efc
raglandconnor <raglandconnor@ufl.edu>
2024-06-16 08:58:21 -0400

    Added contact page, finished resume page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,26 @@ function Contact() {
   return (
     <div>
       <h1 className="mb-16">Contact</h1>
+      <div className="text-xl">
+        <p>
+          Email:{" "}
+          <a
+            href="mailto:connor.ragland@hotmail.com"
+            className="hover:text-blue-500"
+          >
+            connor.ragland@hotmail.com
+          </a>
+        </p>
+
+        <p>
+          <a
+            href="https://www.linkedin.com/in/raglandconnor/"
+            className="underline hover:text-blue-500"
+          >
+            LinkedIn
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,6 +4,13 @@ function Resume() {
   return (
     <div>
       <h1 className="mb-16">Resume</h1>
+      <iframe
+        src="/resume/raglandconnor_resume.pdf"
+        className="h-screen w-full"
+        style={{ border: "none" }}
+      >
+        Your browser does not support iframes.
+      </iframe>
     </div>
   );
 }
