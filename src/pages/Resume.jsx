7935c8e12a1500ca474d2eb92ae5c32941edf70c
raglandconnor <raglandconnor@ufl.edu>
2024-07-14 21:22:29 -0400

    Updated resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 
 function Resume() {
-  return (
-    <main id="resume" className="mt-32 h-screen xl:w-[1280px]">
-      <h1 className="mb-16 text-center xl:text-left xl:pl-12 pb-8 border-b border-white border-opacity-50">
-        Resume
-      </h1>
-      <section className="px-12">
-        <a
-          href="https://drive.google.com/file/d/1YcdJlgcou1L4XyIiFJ7GFz63tL7_nGCb/view?usp=sharing"
-          target="_blank"
-        >
-          <img
-            src="/resume/raglandconnor_resume.png"
-            alt="Connor Ragland Resume"
-            className="px-4"
-          />
-        </a>
-      </section>
-    </main>
-  );
+    return (
+        <main id="resume" className="mt-32 h-screen xl:w-[1280px]">
+            {/* <h1 className="mb-16 text-center xl:text-left xl:pl-12 pb-8 border-b border-white border-opacity-50">
+                Resume
+            </h1> */}
+            <section className="px-0 md:px-12">
+                <a
+                    href="https://drive.google.com/file/d/1EHQ9vCkAAasGDQEp7M_J65x2g0fLnpDG/view?usp=sharing"
+                    target="_blank"
+                >
+                    <img
+                        src="/resume/raglandconnor_resume.png"
+                        alt="Connor Ragland Resume"
+                        className="px-4"
+                        draggable="false"
+                    />
+                </a>
+            </section>
+        </main>
+    );
 }
 
 export default Resume;
