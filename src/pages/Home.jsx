b67ccae7fa65f5ca4738e7b660d8825829efeaaf
raglandconnor <raglandconnor@ufl.edu>
2024-07-25 15:52:41 -0400

    Add experience section to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import About from "../components/About";
-import Skills from "../components/Skills";
-import Projects from "../components/Projects";
-import Education from "../components/Education";
-import Experience from "../components/Experience";
+import About from '../components/About';
+import Skills from '../components/Skills';
+import Projects from '../components/Projects';
+import Education from '../components/Education';
+import Experience from '../components/Experience';
 
 function Home() {
-    return (
-        <main className="flex flex-col mt-24 xl:w-[1280px]">
-            <About />
-            <Skills />
-            {/* <Experience /> */}
-            <Education />
-            <Projects />
-        </main>
-    );
+  return (
+    <main className="flex flex-col mt-24 xl:w-[1280px]">
+      <About />
+      <Skills />
+      <Experience />
+      <Education />
+      <Projects />
+    </main>
+  );
 }
 
 export default Home;
